332898cbcf698b52bd09169d25946c806e80b728
Guillermo <gremesalgarcia@gmail.com>
2023-11-16 19:36:17 +0100

    edicion de fotos

---
+++ Edicion de fotos/Edicion de fotos.py
@@ -0,0 +1,24 @@
+import cv2
+
+img = cv2.imread('mastermind_logorigin.png')
+
+texto = "Equipo 1"
+position = (105, 50)
+font = cv2.FONT_HERSHEY_DUPLEX
+tamaño = 2
+color = (29, 152, 248)
+grosor = 3
+
+cv2.putText(img, texto, position, font, tamaño, color, grosor)
+
+texto = "1DAM Curso 2023/24"
+position = (70, 310)
+font = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
+tamaño = 1.05
+color = (37, 40, 219)
+grosor = 2
+
+cv2.putText(img, texto, position, font, tamaño, color, grosor)
+
+cv2.imwrite("fotocontexto.png", img)
+
