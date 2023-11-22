0c9e75f2fa8094539d2fffa50e115325e4bc3b2b
Ricardo <rtovarfernandez@safareyes.es>
2023-11-22 18:53:37 +0100

    Edicion fotos final

--- Edicion de fotos/Edicion de fotos.py
+++ Edicion de fotos/Edicion de fotos.py
@@ -1,24 +1,27 @@
 import cv2
+def opcion1():
+    img = cv2.imread('mastermind_logorigin.png')
 
-img = cv2.imread('mastermind_logorigin.png')
+    texto = "Equipo 1"
+    position = (105, 50)
+    font = cv2.FONT_HERSHEY_DUPLEX
+    tamaño = 2
+    color = (29, 152, 248)
+    grosor = 3
 
-texto = "Equipo 1"
-position = (105, 50)
-font = cv2.FONT_HERSHEY_DUPLEX
-tamaño = 2
-color = (29, 152, 248)
-grosor = 3
+    cv2.putText(img, texto, position, font, tamaño, color, grosor)
 
-cv2.putText(img, texto, position, font, tamaño, color, grosor)
+    texto = "1DAM Curso 2023/24"
+    position = (70, 310)
+    font = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
+    tamaño = 1.05
+    color = (37, 40, 219)
+    grosor = 2
 
-texto = "1DAM Curso 2023/24"
-position = (70, 310)
-font = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
-tamaño = 1.05
-color = (37, 40, 219)
-grosor = 2
+    cv2.putText(img, texto, position, font, tamaño, color, grosor)
 
-cv2.putText(img, texto, position, font, tamaño, color, grosor)
+    cv2.imshow("Imagen con Texto", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.imwrite("fotocontexto.png", img)
 
