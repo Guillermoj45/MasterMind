03c9cb1d5a9d9327b5760ff03c0389c15e14f5f4
Ricardo <rtovarfernandez@safareyes.es>
2023-11-22 16:34:12 +0100

    Menu

---
+++ Inicio.py
@@ -0,0 +1,25 @@
+
+
+salir = False
+
+while not salir:
+    print('\t\t\t\t\033[1mAPLICACIÓN MASTERMIND\033[0m')
+    print('\t1) Cración del logo de equipo')
+    print('\t2) Generación y ocultado de la combinación')
+    print('\t3) Juego Mastermind')
+    print('\t4) Ranking de récords')
+    print('\t5) Informe de las partidas (PDF)')
+    print('\t6) Salir')
+    opcion = int(input('\tOpción:__ '))
+
+    if opcion == 1:
+        print('\n\t\t\t\t\tOpción: 1\n')
+
+    elif opcion == 2:
+        print('\n\t\t\t\t\tOpción: 2\n')
+
+    elif opcion == 3:
+        print('\n\t\t\t\t\tOpción: 3\n')
+
+    elif opcion == 4:
+        salir = True
