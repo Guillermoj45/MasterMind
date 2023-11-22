80ff593a5d9d82fd3c601c2a89f06cc6743be11b
Ricardo <rtovarfernandez@safareyes.es>
2023-11-22 19:17:08 +0100

    inicio prueba3

--- Inicio.py
+++ Inicio.py
@@ -1,4 +1,34 @@
 import random
+import cv2
+
+
+def opcion1():
+
+    img = cv2.imread('mastermind_logorigin.png')
+
+    texto = "Equipo 1"
+    position = (105, 50)
+    font = cv2.FONT_HERSHEY_DUPLEX
+    tamaño = 2
+    color = (29, 152, 248)
+    grosor = 3
+
+    cv2.putText(img, texto, position, font, tamaño, color, grosor)
+
+    texto = "1DAM Curso 2023/24"
+    position = (70, 310)
+    font = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
+    tamaño = 1.05
+    color = (37, 40, 219)
+    grosor = 2
+
+    cv2.putText(img, texto, position, font, tamaño, color, grosor)
+
+    cv2.imshow("Imagen con Texto", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 def opcion2():
     juego = input('Escribe a qué modalidad de juego deseas jugar: secuencia de cinco números (N) o palabra de ocho'
                   'caracteres (L). Escribe N o L: ')
@@ -8,7 +38,6 @@ def opcion2():
     if juego == 'N':
         numero = random.randint(1000, 9999)
         print(numero)
-
     else:
         palabra_ale = random.choice
         with open('palabras.dat', 'r', encoding='utf-8') as archivo:
@@ -31,7 +60,7 @@ while not salir:
 
     if opcion == 1:
         print('\n\t\t\t\t\tOpción: 1\n')
-
+        opcion1()
     elif opcion == 2:
         print('\n\t\t\t\t\tOpción: 2\n')
         opcion2()
