b16299c3297196e2e8d24e34cc5debf228638a8b
Ricardo <rtovarfernandez@safareyes.es>
2023-12-03 13:55:16 +0100

    ranking false

--- Inicio.py
+++ Inicio.py
@@ -10,6 +10,10 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib import utils, colors
 from reportlab.lib.styles import getSampleStyleSheet
 
+import itertools
+from random import randint
+from statistics import mean
+from reportlab.lib.pagesizes import A4
 
 def opcion1():
     img = cv2.imread('mastermind_logorigin.png')
@@ -240,6 +244,43 @@ def Rankins():
     print(datosor.to_string(index=False, col_space=10, justify='center'))
 
 
+def grouper(iterable, n):
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args)
+
+
+def export_to_pdf(data):
+    c = canvas.Canvas("grilla-alumnos.pdf", pagesize=A4)
+    w, h = A4
+    max_rows_per_page = 45
+    # Margin.
+    x_offset = 50
+    y_offset = 50
+    # Space between rows.
+    padding = 15
+
+    xlist = [x + x_offset for x in [0, 200, 250, 300, 350, 400, 480]]
+    ylist = [h - y_offset - i * padding for i in range(max_rows_per_page + 1)]
+
+    for rows in grouper(data, max_rows_per_page):
+        rows = tuple(filter(bool, rows))
+        c.grid(xlist, ylist[:len(rows) + 1])
+        for y, row in zip(ylist[:-1], rows):
+            for x, cell in zip(xlist, row):
+                c.drawString(x + 2, y - padding + 3, str(cell))
+        c.showPage()
+
+    c.save()
+
+
+data = [("NOMBRE", "NOTA 1", "NOTA 2", "NOTA 3", "PROM.", "ESTADO")]
+for i in range(1, 101):
+    exams = [randint(0, 10) for _ in range(3)]
+    avg = round(mean(exams), 2)
+    state = "Aprobado" if avg >= 4 else "Desaprobado"
+    data.append((f"Alumno {i}", *exams, avg, state))
+export_to_pdf(data)
+
 def PDF():
     c = canvas.Canvas("partidas.pdf", pagesize=letter)
 
@@ -260,8 +301,10 @@ def PDF():
     c.setFont("Helvetica", 12)
     c.drawString(60, 530, f"El jugador pedro ha jugado las siguientes partidas")
 
+
     # Guardar el PDF
     c.save()
+
 
 
 salir = False
@@ -301,4 +344,4 @@ while not salir:
         PDF()
 
     elif opcion == 6:
-        salir = True
+        export_to_pdf(data)
